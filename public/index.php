d3a0f0576c33d05db1554c52c456a8a7edf32802
sophiedannery <dannery.sophie@gmail.com>
2025-08-02 17:02:29 +0200

    fix index

--- public/index.php
+++ public/index.php
@@ -1,22 +1,16 @@
 <?php
 
-if (($_SERVER['APP_ENV'] ?? 'prod') === 'prod') {
-    $host = $_SERVER['HTTP_HOST'] ?? '';
-    $requestUri = $_SERVER['REQUEST_URI'] ?? '';
+$host = $_SERVER['HTTP_HOST'] ?? '';
+$requestUri = $_SERVER['REQUEST_URI'] ?? '';
 
-    if (
-        (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') ||
-        ($host !== 'www.ohpetardfestival.com')
-    ) {
-        header('Location: https://www.ohpetardfestival.com' . $requestUri, true, 301);
-        exit();
-    }
+if ($host !== 'www.ohpetardfestival.com') {
+    header('Location: http://www.ohpetardfestival.com' . $requestUri, true, 301);
+    exit();
 }
 
 use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
